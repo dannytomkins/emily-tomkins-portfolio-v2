d6df7e530fd50be4c102061a7c04e9d34a5866f7
Danny Tomkins <tomkinsdan@gmail.com>
2026-03-23 21:29:06 -0500

    removed optimisticsortorder, belived to be causing hydration issues

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -11,8 +11,8 @@ export interface HomePageProps {
   data: HomePageQueryResult | null
 }
 
-export async function HomePage({data}: HomePageProps) {
-  // Default to an empty object to allow previews on non-existent documents
+export function HomePage({data}: HomePageProps) {
+    // Default to an empty object to allow previews on non-existent documents
   const {overview = [], showcaseProjects = [], title = ''} = data ?? {}
 
   const dataAttribute =
@@ -38,28 +38,26 @@ export async function HomePage({data}: HomePageProps) {
         />
       )}
       {/* Showcase projects */}
-      <div className="mx-auto max-w-[100rem] rounded-md border">
-        <OptimisticSortOrder id={data?._id} path={'showcaseProjects'}>
-          {showcaseProjects &&
-            showcaseProjects.length > 0 &&
-            showcaseProjects.map((project) => {
-              const href = resolveHref(project?._type, project?.slug)
-              if (!href) {
-                return null
-              }
-              return (
-                <Link
-                  className="flex flex-col gap-x-5 p-2 transition odd:border-b odd:border-t hover:bg-gray-50/50 xl:flex-row odd:xl:flex-row-reverse"
-                  key={project._key}
-                  href={href}
-                  data-sanity={dataAttribute?.(['showcaseProjects', {_key: project._key}])}
-                >
-                  <ProjectListItem project={project as any} />
-                </Link>
-              )
-            })}
-        </OptimisticSortOrder>
-      </div>
+<div className="mx-auto max-w-[100rem] rounded-md border">
+  {showcaseProjects &&
+    showcaseProjects.length > 0 &&
+    showcaseProjects.map((project) => {
+      const href = resolveHref(project?._type, project?.slug)
+      if (!href) {
+        return null
+      }
+      return (
+        <Link
+          className="flex flex-col gap-x-5 p-2 transition odd:border-b odd:border-t hover:bg-gray-50/50 xl:flex-row odd:xl:flex-row-reverse"
+          key={project._key}
+          href={href}
+          data-sanity={dataAttribute?.(['showcaseProjects', {_key: project._key}])}
+        >
+          <ProjectListItem project={project as any} />
+        </Link>
+      )
+    })}
+</div>
     </div>
   )
 }
