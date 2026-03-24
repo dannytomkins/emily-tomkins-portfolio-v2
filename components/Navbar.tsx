f706f11b694bab164ba7b7741dc4dde128e9a03e
Danny Tomkins <tomkinsdan@gmail.com>
2026-03-23 21:39:26 -0500

    removed optimistic sort order in navbar. it created issues editing in studio

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -53,35 +53,33 @@ export function Navbar(props: NavbarProps) {
       : null
 
   return (
-    <header
-      className="sticky top-0 z-10 flex flex-wrap items-center gap-x-5 bg-white/80 px-4 py-4 backdrop-blur md:px-16 md:py-5 lg:px-32"
-      data-sanity={dataAttribute?.('menuItems')}
-    >
-      <OptimisticSortOrder id={data?._id} path="menuItems">
-        {data?.menuItems?.map((menuItem) => {
-          const href = resolveMenuItemHref(menuItem)
-          if (!href) return null
+<header
+  className="sticky top-0 z-10 flex flex-wrap items-center gap-x-5 bg-white/80 px-4 py-4 backdrop-blur md:px-16 md:py-5 lg:px-32"
+  data-sanity={dataAttribute?.('menuItems')}
+>
+  {data?.menuItems?.map((menuItem) => {
+    const href = resolveMenuItemHref(menuItem)
+    if (!href) return null
 
-          const isHome = isHomeMenuItem(menuItem)
-          const isExternal = menuItem?.kind === 'external'
-          const label = stegaClean(menuItem?.title) ?? 'Untitled'
+    const isHome = isHomeMenuItem(menuItem)
+    const isExternal = menuItem?.kind === 'external'
+    const label = stegaClean(menuItem?.title) ?? 'Untitled'
 
-          return (
-            <Link
-              key={menuItem._key}
-              className={`text-lg hover:text-black md:text-xl ${
-                isHome ? 'font-extrabold text-black' : 'text-gray-600'
-              }`}
-              data-sanity={dataAttribute?.(['menuItems', {_key: menuItem._key as string}])}
-              href={href}
-              target={isExternal && menuItem?.openInNewTab ? '_blank' : undefined}
-              rel={isExternal && menuItem?.openInNewTab ? 'noreferrer' : undefined}
-            >
-              {label}
-            </Link>
-          )
-        })}
-      </OptimisticSortOrder>
-    </header>
+    return (
+      <Link
+        key={menuItem._key}
+        className={`text-lg hover:text-black md:text-xl ${
+          isHome ? 'font-extrabold text-black' : 'text-gray-600'
+        }`}
+        data-sanity={dataAttribute?.(['menuItems', {_key: menuItem._key as string}])}
+        href={href}
+        target={isExternal && menuItem?.openInNewTab ? '_blank' : undefined}
+        rel={isExternal && menuItem?.openInNewTab ? 'noreferrer' : undefined}
+      >
+        {label}
+      </Link>
+    )
+  })}
+</header>
   )
 }
