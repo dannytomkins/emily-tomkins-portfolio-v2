caebd8820f9bb063a153cb9bdab58c7def732bcb
Danny Tomkins <tomkinsdan@gmail.com>
2026-05-30 17:10:26 -0500

    updated title style to be consistent across gallery pages and about page

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,8 +17,7 @@ export function Header(props: HeaderProps) {
   return (
     <div className={`${centered ? 'text-center' : 'w-5/6 lg:w-3/5'}`}>
       {/* Title */}
-      {title && <div className="text-3xl font-extrabold tracking-tight md:text-5xl">{title}</div>}
-      {/* Description */}
+      {title && <h1 className="text-3xl font-semibold">{title}</h1>} {/* Description */}
       {description && (
         <div className="mt-4 text-pretty font-serif text-xl text-gray-600 md:text-2xl">
           <CustomPortableText id={id} type={type} path={path} value={description} />
